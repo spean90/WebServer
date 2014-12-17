67a49e22b7d7856daf3d17cbaa11d4a2a1ad8320
spean90 <spean90@gmail.com>
2014-12-17 14:50:07 +0800

    添加文件上传功能；

--- src/com/webserver/apicontroller/IUpLoadController.java
+++ src/com/webserver/apicontroller/IUpLoadController.java
@@ -1,8 +1,18 @@
 package com.webserver.apicontroller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("api")
@@ -10,7 +20,21 @@ public class IUpLoadController {
 
 	@RequestMapping("uploadImg.do")
 	@ResponseBody
-	public void uploadImg(){
+	public void uploadImg(HttpServletRequest request,String name,MultipartFile file,MultipartFile file1){
 		System.out.println("....in..uploadImg;...;");
+		System.out.println("name:"+name);
+		System.out.println(file.getSize());
+		String basepath = request.getServletContext().getRealPath("upload");
+		System.out.println(basepath);
+		System.out.println(file.getOriginalFilename());
+		String fileName = file.getOriginalFilename();
+//		try {
+//			File f = new File(basepath+"/"+fileName);
+//			FileOutputStream fout = new FileOutputStream(f);
+//			fout.write(file.getBytes());
+//			fout.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
