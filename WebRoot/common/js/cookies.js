d9459bdc035dbbce6f489df1b5e61d074aa70ba6
spean90 <spean90@gmail.com>
2015-01-14 10:27:19 +0800

    修改密码和退出是把session内user置空

--- WebRoot/common/js/cookies.js
+++ WebRoot/common/js/cookies.js
@@ -6,9 +6,11 @@ Cookies.clear = function(name, options) {
 	document.cookie = name + "=" + "; expires=Thu, 01-Jan-70 00:00:01 GMT; domain=" + domain + "; path=" + path;
 };
 Cookies.removeItem = function (sKey, sPath, sDomain) {
-    if (!sKey || !this.hasItem(sKey)) { return false; }
+    if (!sKey || !this.hasItem(sKey)) {return false; }
     document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + ( sDomain ? "; domain=" + sDomain : "") + ( sPath ? "; path=" + sPath : "");
     return true;
   };
 
-
+Cookies.hasItem = function (sKey) {
+	    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+	  };
