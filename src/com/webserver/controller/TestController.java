70c4699143b913297b31a1d7062e55c2f13d2873
spean90 <spean90@gmail.com>
2015-03-10 11:56:12 +0800

    修改拦截.html的请求、完成url静态化

--- src/com/webserver/controller/TestController.java
+++ src/com/webserver/controller/TestController.java
@@ -12,14 +12,14 @@ public class TestController {
 	public ModelAndView regularMapping(@PathVariable(value="pid") String pid) {
 		ModelAndView modelAndView = new ModelAndView("detail");
 		System.out.println("++++++++++++++++"+pid);
-		modelAndView.addObject("pid", pid); 
+		modelAndView.addObject("pid", "你是大放送"); 
 		return modelAndView;
 	}
 	
 	@RequestMapping(value="/indexPage")
 	public ModelAndView index() {
 		System.out.println(">>>>>>>>>>>>>>>>>>");
-		ModelAndView modelAndView = new ModelAndView("login");
+		ModelAndView modelAndView = new ModelAndView("detail");
 		return modelAndView;
 	}
 }
