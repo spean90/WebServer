4a12890176ee7beb73915a0e6b4988d98699702b
hsp <spean90@gmail.com>
2015-05-22 12:31:32 +0800

    ...
    ..

--- WebRoot/common/js/sys.js
+++ WebRoot/common/js/sys.js
@@ -1,3 +1,11 @@
 var Sys = {
-		user : {}
+		user : {},
+		ctx : Sys.getContextPath(),
+		getContextPath : function (){ 
+			var pathName = document.location.pathname; 
+			var index = pathName.substr(1).indexOf("/"); 
+			var result = pathName.substr(0,index+1); 
+			alert(result);
+			return result; 
+		} 
 }
