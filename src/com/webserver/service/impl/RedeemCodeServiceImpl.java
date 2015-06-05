911ddecf8e06e30ede3c0b6816562cab75845396
hsp <spean90@gmail.com>
2015-06-05 15:24:26 +0800

    发送优惠券礼包。生成news

--- src/com/webserver/service/impl/RedeemCodeServiceImpl.java
+++ src/com/webserver/service/impl/RedeemCodeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.webserver.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -7,9 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Service;
 
+import com.webserver.common.util.DateUtil;
 import com.webserver.common.util.StringUtil;
+import com.webserver.dao.NewsDao;
 import com.webserver.dao.OperLogDao;
 import com.webserver.dao.RedeemCodeDao;
+import com.webserver.modal.News;
 import com.webserver.modal.OperLog;
 import com.webserver.modal.RedeemCode;
 import com.webserver.service.IRedeemCodeService;
@@ -20,12 +24,20 @@ public class RedeemCodeServiceImpl implements IRedeemCodeService {
 	private RedeemCodeDao redeemCodeDao;
 	@Resource
 	private OperLogDao operLogDao;
+	@Resource
+	private NewsDao newsDao;
 	@Override
 	public void addRedeemCode(RedeemCode redeemCode,HttpServletRequest request) {
 		OperLog operLog = new OperLog(request);
 		operLog.setOperAction("发送优惠礼包"+redeemCode.getCouponPackageIds());
 		operLog.setParams(StringUtil.toJson(redeemCode));
 		redeemCodeDao.addRedeemCode(redeemCode);
+		News news = new News();
+		news.setUserId(redeemCode.getUserId());
+		news.setCreateTime(DateUtil.getDateTimeString(new Date()));
+		news.setStatus(0);
+		news.setContent(redeemCode.getRedeemCode());
+		newsDao.addNews(news);
 		operLogDao.addLog(operLog);
 	}
 
