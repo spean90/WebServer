420aa04abda797d22a0e01f9bd06e737a0dee901
spean90 <spean90@gmail.com>
2015-06-15 23:17:34 +0800

    Revert "2015-06-15"
    This reverts commit 9ef7050223105f6f145b9f109b32b807733ee26f.

--- WebRoot/common/js/modal.js
+++ WebRoot/common/js/modal.js
@@ -20,11 +20,11 @@ var Modal = {
 			data : config.data,
 			async: config.async || false,
 			success : function(data) {
-				data = JSON.parse(data);
+				data = JSON.StrToJSON(data);
 				var success = data.success;
 				
 				if(success == false) {
-					alert("操作失败");
+					Modal.showError(data.message || "操作失败", "", function() {});
 				}
 				else {
 					config.success(data);
@@ -33,44 +33,21 @@ var Modal = {
 			error : function(XMLHttpRequest, textStatus, errorThrown) {
 				var status = XMLHttpRequest.status;
 				if(status == 401) {
-					alert("操作失败");
+					Modal.showError(Modal.status[status], "错误提示", function() {
+						window.location.href = "/login.html";
+					});
 				}
 				else if(status == 403) {
-					window.location.href = "/login.html";
+					Modal.showError(Modal.status[status], "错误提示", function() {
+						window.location.href = "/login.html";
+					});
 				} else {
-					alert("操作失败");
+					Modal.showError( Modal.status[status]||"服务器异常");
 				}
 			}
 		});
 	},
 
-	jsonp : function(config){
-		$.jsonp({
-			url : config.url,
-			data : config.data,
-			callbackParameter: "callback",
-			success : function(data){ 
-				config.success(data);
-			},
-			complete: function() {
-	          // 数据获取完成后，需要做的事，此为隐藏读取数据的滚动条。
-	        },
-	        error : function(xOptions, textStatus) {
-	        	console.log(xOptions);
-				//alert(textStatus);
-				//window.location.href = "/index.html";
-			}
-		});
-	},
-	
-	alert : function(content,title) {
-		var mytitle = title?title:'友情提示';
-		$('#modal').remove();
-		//$(this).modal('modal',mytitle,content);
-		$(this).hint('modal', mytitle, content);
-	},
-	
-	
 	/**
 	 * 封装的表单submit方法，对操作失败进行了封装处理
 	 * 
@@ -84,9 +61,9 @@ var Modal = {
 				return $(this).form('validate');
 			},
 			success : function(data) {
-				data = JSON.parse(data);
+				data = JSON.StrToJSON(data);
 				if (data.success == false) {
-					alert("操作失败");
+					Modal.showError(data.message || "操作失败", "", function() {});
 				} else {
 					config.success(data);
 				}
@@ -103,34 +80,68 @@ var Modal = {
 			});
 		}*/
 	},
-	/*
-	 * 验证手机号码
-	 * 验证规则：11位数字，以1开头。
+
+	/**
+	 * 弹出普通提示层
+	 * 
+	 * @param msg 提示内容
+	 * @param titie 提示标题
 	 */
-	checkMobile : function(str) {
-	    var re = /^1\d{10}$/
-	    if (re.test(str)) {
-	        //alert("正确");
-	    	return 0;
-	    } else {
-	        //alert("错误");
-	    	return 1;
-	    }
+	showAlert : function(msg, title) {
+		$.messager.alert(title || '操作提示', msg, 'info');
 	},
-	/*
-	 * 验证邮箱
-	 * 验证规则：姑且把邮箱地址分成“第一部分@第二部分”这样
-	 * 第一部分：由字母、数字、下划线、短线“-”、点号“.”组成，
-	 * 第二部分：为一个域名，域名由字母、数字、短线“-”、域名后缀组成，而域名后缀一般为.xxx或.xxx.xx，一区的域名后缀一般为2-4位，如cn,com,net，现在域名有的也会大于4位
+
+	/**
+	 * 弹出错误提示层
+	 * 
+	 * @param msg 提示内容
+	 * @param titie 提示标题
 	 */
-	checkEmail : function(str){
-		var re = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
-		if(re.test(str)){
-			//alert("正确");
-			return 0;
-		}else{
-			//alert("错误");
-			return 1;
-		}
+	showError : function(msg, titie, fuc) {
+		$.messager.alert(titie || '错误提示', msg, 'error', fuc);
+	},
+
+	/**
+	 * 弹出确认提示层
+	 * 
+	 * @param titie 提示标题
+	 * @param msg 提示内容
+	 * @param fuc 点击确定后执行的函数
+	 */
+	showConfirm : function(msg,titie, fuc) {
+		$.messager.alert(titie || '提示', msg, 'info', fuc);
+	},
+	
+	/**
+	 * 无父页面则跳转无权限页面，不是刷新父页面
+	 */
+	gotoPermissionPage:function(status){
+		if (window.top!=window.self) {
+			Modal.showError(Modal.status[status], "错误提示", function() {
+				window.parent.location.reload();
+			});
+		}else{ 
+			Modal.showError(Modal.status[status], "错误提示", function() {
+				window.location.href = "/login.html";
+			});
+		 } 
 	}
+	
 };
+
+
+//跨域iframe高度自适应  begin
+function initFrameHeight() {
+	var pdiv = window.parent.document.getElementById('mainDiv');
+	pdiv.style.height = document.body.scrollHeight+'px';
+	
+	var piframe = window.parent.document.getElementById('centerIFrame');
+	piframe.style.height = (document.body.scrollHeight-20)+'px';
+}
+
+$(function() {
+	//跨域iframe高度自适应  begin
+	window.onload = initFrameHeight;
+});
+
+
