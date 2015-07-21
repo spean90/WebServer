050004ad22b94529631865e3a37c1153d25bc79d
siping_huang@foxitsoftware.com <siping_huang@foxitsoftware.com>
2015-07-21 14:25:02 +0800

    修改js为动态配置路径

--- WebRoot/common/js/sys.js
+++ WebRoot/common/js/sys.js
@@ -1,8 +1,9 @@
 var Sys = {
 		serviceDomain : 'http://120.26.48.53:8080/ehuishou',
-		//serviceDomain : 'http://127.0.0.1/'
+		//serviceDomain : 'http://srv.ehuishou.com',
 			
-		staticDomain : 'http://127.0.0.1/'
+		//staticDomain : 'http://127.0.0.1:81/',
+		staticDomain : 'http://s.ehuishou.com/'
 }
 
 
